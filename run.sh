8a5856b1f38b9e276978936aadf2f0083ec0eeff
Ivan Pedrazas <ipedrazas@gmail.com>
2014-12-01 01:09:21 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 BASE_LOG=$(mount | grep /dev/mapper/ubuntu--vg-root | awk '{print $3}' | tail -n +4 | sed 's/$/\/*/' | tr "\\n" " ")
 
-echo "Running Tailon as python tailon -b 0.0.0.0:8084 -f /var/log/* $BASE_LOG"
-python tailon -b 0.0.0.0:8084 -f /var/log/* $BASE_LOG
+echo "Running Tailon as python tailon -b 0.0.0.0:8084 -f $BASE_LOG"
+python tailon -b 0.0.0.0:8084 -f $BASE_LOG
 
