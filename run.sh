a7c15b57fa82e4b605c5056402c7d59cdfba56e1
Ivan Pedrazas <ipedrazas@gmail.com>
2014-12-07 23:08:04 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-BASE_LOG=$(mount | grep /dev/ | awk '{print $3}' | tail -n +4 | sed 's/$/\/*/' | tr "\\n" " ")
+# BASE_LOG=$(mount | grep /dev/ | awk '{print $3}' | tail -n +4 | sed 's/$/\/*/' | tr "\\n" " ")
 
-echo "Running Tailon as python tailon -b 0.0.0.0:8084 -f $BASE_LOG"
-tailon -b 0.0.0.0:8084 -f  $BASE_LOG
+echo "Running Tailon as python tailon -b 0.0.0.0:8084 -f $BASE"
+tailon -b 0.0.0.0:8084 -f  $BASE
 
 
